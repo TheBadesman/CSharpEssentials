4d740c9c88963653617751d1a7910b491ec43932
TheBadesman <Cameron@ottleys.net>
2022-09-10 17:28:36 -0600

    Update ClassExample.cs

--- Basics/001 Conventions and Syntax/ClassExample.cs
+++ Basics/001 Conventions and Syntax/ClassExample.cs
@@ -1,5 +1,6 @@
 //Use this file to store your work to demonstrate your understanding of operators
 //Make sure your work is different from the instructors examples or your peers
+using System;
 
 public class ClassExample
 {
@@ -10,12 +11,21 @@ public class ClassWithVariables
 {
     public int number = 2;
     public string userName = "Bob";
+    public string kingKiller = "The king killer was beaten, but his return has already been fortold";
 }
 
 public class ClassWithFunctions
 {
+
+    public ClassWithVariables strings;
+
     public void RunFunction()
     {
         Console.WriteLine("Running Function");
+    }
+
+    public void BetterFunction()
+    {
+        Console.WriteLine(strings.kingKiller);
     }
 }   
