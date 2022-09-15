0b691449f2c3304b774d2d6efbd5787763bdc6ea
TheBadesman <Cameron@ottleys.net>
2022-09-14 23:11:49 -0600

    Update SwitchesLab.cs
    Did the switches lab part

--- Basics/002FunctionsAndConditionals/SwitchesLab.cs
+++ Basics/002FunctionsAndConditionals/SwitchesLab.cs
@@ -9,6 +9,55 @@ public class Program
 		gameStates = new GameStates();
 		gameStates.currentState = GameStates.States.Ending;
 		gameStates.CheckState();
+
+		switch (week) {
+   		case 55:
+    		printf("Fifty five weeks since they last came");
+    	break;
+    	case 56:
+    		printf("Fifty six weeks since they last came");
+    	break;
+    	case 57:
+			printf("Fifty seven weeks since they last came");
+    	break;
+    	case 58:
+    		printf("Fifty eight weeks since they last came");
+    	break;
+    	case 59:
+      		printf("Fifty nine weeks since they last came");
+    	break;
+    	case 60:
+			printf("sixty weeks until since they last came");
+    	break;
+    	case 0:
+    		printf("HERE THEY COME");
+    	break;
+		
+
+				switch (week) {
+   		case 55:
+    		printf("Fifty five weeks since they last came");
+    	break;
+    	case 56:
+    		printf("Fifty six weeks since they last came");
+    	break;
+    	case 57:
+			printf("Fifty seven weeks since they last came");
+    	break;
+    	case 58:
+    		printf("Fifty eight weeks since they last came");
+    	break;
+    	case 59:
+      		printf("Fifty nine weeks since they last came");
+    	break;
+    	case 60:
+			printf("sixty weeks until since they last came");
+    	break;
+    	default:
+    		printf("HERE THEY COME");
+    	break;
+  		}
+  }
 	}
 }
 
